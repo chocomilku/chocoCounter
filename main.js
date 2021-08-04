e510372e28eba58d2fdb98581990683a62ce2413
Andrei <andrei.berja078@gmail.com>
2021-08-04 13:54:30 +0800

    added fitty.min.js for title text fit
    fits text to its container

---
+++ main.js
@@ -0,0 +1,2 @@
+fitty('#title', {minSize:16, maxSize: 50, multiLine: true});
+document.getElementById('title').innerText = "Aqours - Todokanai Hoshi Da To Shitemo [1, 2, Sunshine] (chocomilku-) +MRFI";
